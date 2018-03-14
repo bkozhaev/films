13d179990e203ecbf9909326fe22610b364fba2e
baur <bkozhaev@gmail.com>
2018-03-14 09:19:49 +0400

    adding before ot spec_film.rb and deleting an empty line

--- spec/film_spec.rb
+++ spec/film_spec.rb
@@ -3,20 +3,18 @@
 require_relative '../lib/film.rb'
 
 describe 'Film.rb' do
-
+  before(:all) do
+    @film = Film.from_path(__dir__ + "/fixtures/01.txt")
+  end
   it 'return data from txt file ' do
-    film = Film.from_path(__dir__ + "/fixtures/01.txt")
-
-    expect(film.name).to eq('Список Шиндлера')
-    expect(film.director).to eq('Стивен Спилберг')
-    expect(film.year).to eq('1993')
+    expect(@film.name).to eq('Список Шиндлера')
+    expect(@film.director).to eq('Стивен Спилберг')
+    expect(@film.year).to eq('1993')
   end
 
   describe 'to_s' do
     it 'returns the data from file in expected order' do
-      film = Film.from_path(__dir__ + "/fixtures/01.txt")
-
-      expect(film.to_s).to eq('Стивен Спилберг - Список Шиндлера (1993)')
+      expect(@film.to_s).to eq('Стивен Спилберг - Список Шиндлера (1993)')
     end
   end
 end
